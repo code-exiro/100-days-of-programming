36528e6e5c4352fe25b71295585716b336964be8
erizo <nandezemir@gmail.com>
2024-11-28 01:58:08 -0600

    Dia 2: Anagrama

---
+++ dia_02/main.py
@@ -0,0 +1,14 @@
+# Es un anagrama?
+
+# Escribe una función que reciba dos palabras (String) y retorne
+# verdadero o falso (Bool) según sean o no anagramas.
+# Un Anagrama consiste en formar una palabra reordenando TODAS
+# las letras de otra palabra inicial.
+# NO hace falta comprobar que ambas palabras existan.
+# Dos palabras exactamente iguales no son anagrama.
+
+x = [1, 2]
+y = [2, 1]
+x is y  
+if x == y:
+    print('Verdad')
